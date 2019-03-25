f6fc1d37ecac2881c330f87add287c3e0e9bb16a
Boban.S <boban.sugareski@gsix.me>
2019-03-25 19:32:43 +0100

    Adding parameterized tests

--- src/test/java/io/bobz/PalindromGeneratorServiceTest.java
+++ src/test/java/io/bobz/PalindromGeneratorServiceTest.java
@@ -3,6 +3,8 @@ package io.bobz;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +21,20 @@ class PalindromeGeneratorServiceTest {
     @DisplayName("generate method")
     void testGenerate() {
         String original = "arsenal";
+
+        String palindrome = generatePalindrome(original);
+
+        assertEquals(palindrome, palindromeGeneratorService.generate(original));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "barcelona",
+            "arsenal",
+            "celtic"
+    })
+    @DisplayName("parameterized generate method")
+    void parameterizedTestGenerate(String original) {
 
         String palindrome = generatePalindrome(original);
 
